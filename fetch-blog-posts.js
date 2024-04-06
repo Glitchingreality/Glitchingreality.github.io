4fb92a94dd023833f9e00e6f4b1470de70a2c612
Glitchingreality <Glitchingreality@gmail.com>
2024-04-06 12:23:19 -0400

    fix
    fix

--- fetch-blog-posts.js
+++ fetch-blog-posts.js
@@ -41,7 +41,7 @@
 
     <script>
         // Fetch blog posts from GitHub repository
-        fetch('https://api.github.com/repos/glitchingreality/blog/contents/posts1.md')
+        fetch('https://api.github.com/repos/glitchingreality/glitchingreality.github.io/contents/blog/posts1.md')
           .then(response => response.json())
           .then(posts => {
             // Filter out non-Markdown files
